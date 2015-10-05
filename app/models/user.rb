9ffbd9a0584c66a6b7a8eda667599d34d21116f1
RSR312-206 <roccosummer@gmail.com>
2015-10-05 15:51:22 -0700

    fixed typeo

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,14 @@
 class User < ActiveRecord::Base
   validates :first_name, :last_name,
   presence: true,
-  length: {minimum: 4}
-  excluded_names = ["Elie Schoppik", "Colt Steele", "Tim Garcia", "Chirs Burkhart"],
-  validate :user_exclusions
+  length: {minimum: 4},
+  validate: user_exclusions
 
   private
-
   def user_exclusions
+  excluded_names = ["Elie Schoppik", "Colt Steele", "Tim Garcia", "Chris Burkhart"]
     excluded_names.each do |name|
-      if name.split(' ')[0] == :first_name && name.split(' ')[1] == :last_name
+      if name.split(' ')[0] == first_name && name.split(' ')[1] == last_name
         errors.add(first_name: "this is a restricted name")
         errors.add(last_name: "this is a restricted name")
       end
